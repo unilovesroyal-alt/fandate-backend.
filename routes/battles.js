87c172eaeb8aa12c610945d2ff7f67c5dbda54cf
unilovesroyal-alt <unilovesroyal@gmail.com>
2026-04-26 21:06:11 -0500

    Create battles.js

---
+++ routes/battles.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// Placeholder for battle routes
+router.get('/', (req, res) => {
+  res.json({ message: 'Colts Game battles coming soon' });
+});
+
+module.exports = router;
